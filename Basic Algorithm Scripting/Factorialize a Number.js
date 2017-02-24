d2e2742998ffb2015b5e9011380ae8b4738af02a
Randell Dawson <rdawson@onepathtech.com>
2017-02-24 00:46:33 -0800

    added Reverse a String and Factorialize a Number

---
+++ Basic Algorithm Scripting/Factorialize a Number.js
@@ -0,0 +1,23 @@
+/* Bonfire: Factorialize a Number
+Return the factorial of the provided integer.
+
+If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+
+Factorials are often represented with the shorthand notation n!
+
+For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+Code by: Randell Dawson
+http://www.freecodecamp.com/rmdawson71
+*/
+
+function factorialize(num) {
+  if (num === 0 || num === 1) return 1;
+  product = 1;
+  for (i = 2; i < num + 1; i++) {
+    product *= i;
+  }
+  return product;
+}
+
+factorialize(5);
---
+++ Basic Algorithm Scripting/Reverse a String.js
@@ -0,0 +1,15 @@
+/* Bonfire: Reverse a String
+Reverse the provided string.
+
+You may need to turn the string into an array before you can reverse it.
+
+Your result must be a string.
+
+Code by: Randell Dawson
+http://www.freecodecamp.com/rmdawson71
+*/
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+reverseString("hello");
