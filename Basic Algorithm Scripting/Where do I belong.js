8a10270d5e55f4fe9a2578852bd73e504b2cced3
Randell Dawson <rdawson@onepathtech.com>
2017-04-14 15:32:27 -0700

    created a functinal one-liner

--- Basic Algorithm Scripting/Where do I belong.js
+++ Basic Algorithm Scripting/Where do I belong.js
@@ -6,13 +6,17 @@ Return the lowest index at which a value (second argument) should be inserted in
 For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0),
 but less than 2 (index 1).
 
-Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted 
+Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted
 it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
 
 Code by: Randell Dawson
 http://www.freecodecamp.com/rmdawson71
 */
+function getIndexToIns(arr, num) {
+  return arr.sort((a,b) => a - b).filter(item => num > item).length;
+}
 
+/* original solution
 function getIndexToIns(arr, num) {
   arr.sort( (a, b) => a - b);
   for (i=0;i<arr.length;i++) {
@@ -20,5 +24,5 @@ function getIndexToIns(arr, num) {
   }
   return arr.length;
 }
-
+*/
 getIndexToIns([3, 10, 5], 3);
