3ddefafa20249269cfacb160656aba19ab3840f5
Randell Dawson <rdawson@onepathtech.com>
2017-04-14 15:12:27 -0700

    removed parentheses around word in map argument

--- Basic Algorithm Scripting/Title Case a Sentence.js
+++ Basic Algorithm Scripting/Title Case a Sentence.js
@@ -5,20 +5,20 @@ Return the length of the longest word in the provided sentence.
 Your response should be a number.
 Code by: Randell Dawson
 http://www.freecodecamp.com/rmdawson71
-
- no array version
-function titleCase(str) {
-  var newStr = '';
-  for (var i=0; i<str.length; i++) {
-    if (str[i] !== ' ' && str[i-1] === ' ' || i===0) newStr += str[i].toUpperCase();
-    else newStr += str[i].toLowerCase();
-  }
-  return newStr;
-}
 */
 
 function titleCase(str) {
-  return str.toLowerCase().split(' ').map((word)=> word[0].toUpperCase()+word.slice(1)).join(' ');
+  return str.toLowerCase().split(' ').map(word => word[0].toUpperCase()+word.slice(1)).join(' ');
 }
+
+/*
+ no array version
+ function titleCase(str) {
+   for (var newStr = '', i=0; i<str.length; i++) {
+     newStr += str[i] !== ' ' && str[i-1] === ' ' || i===0 ? str[i].toUpperCase() : str[i].toLowerCase()
+   }
+   return newStr;
+ }
+*/
 
 titleCase("I'm a little tea pot");
