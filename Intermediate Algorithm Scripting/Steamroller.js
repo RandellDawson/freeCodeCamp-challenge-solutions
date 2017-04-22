c9f34774722e4915ce8eefd61cd8e1cacebf60b1
Randell Dawson <rdawson@onepathtech.com>
2017-04-22 00:43:10 -0700

    added an imperative solution which closely mimics the functional solution post for the Adv solution in the FCC wiki

--- Intermediate Algorithm Scripting/Steamroller.js
+++ Intermediate Algorithm Scripting/Steamroller.js
@@ -16,4 +16,22 @@ function steamrollArray(arr) {
   return newArr;
 }
 
+/* my version using a for loop and recursion while trying to emulate the Advanced solution shown
+FCC wiki  which used reduce and recursion  */
+function steamrollArray(arr,flat) {
+  if (!flat)  flat= [];
+  for (var i = 0; i < arr.length; i++) {
+    var toFlatten = arr[i];
+    if (!Array.isArray(toFlatten))  flat.push(toFlatten);
+    else  steamrollArray(toFlatten ,flat);
+  }
+  return flat;
+}
+
+// function steamrollArray(arr) {
+//   return arr.reduce(function (flat, toFlatten) {
+//     return flat.concat(Array.isArray(toFlatten) ? steamrollArray(toFlatten) : toFlatten);
+//   }, []);
+// }
+
 steamrollArray([1, [2], [3, [[4]]]]);
