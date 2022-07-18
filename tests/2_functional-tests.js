9c93a9a3cbdda366f4e244cce3b5a5a126898c53
Randell Dawson <randy@randydawson.com>
2022-07-18 15:06:37 -0600

    fix: finished funtional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -36,7 +36,7 @@ suite('Functional Tests', function () {
       chai
         .request(server)
         .put('/travellers')
-        .send({ "surname": "Colombo"})
+        .send({ "surname": "Colombo" })
         .end(function (err, res) {
           assert.equal(res.status, 200);
           assert.equal(res.type, "application/json");
@@ -48,28 +48,31 @@ suite('Functional Tests', function () {
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
       chai.request(server)
-      .put('/travellers')
-      .send({ "surname": "da Verrazzano", "name": "Giovanni" })
-      .end(function (err, res){
-        assert.equal(res.status, 200);
-        assert.equal(res.type, "application/json");
-        assert.equal(res.body.name, "Giovanni");
-        assert.equal(res.body.surname, "da Verrazzano");
-        done();
-      });
+        .put('/travellers')
+        .send({ "surname": "da Verrazzano", "name": "Giovanni" })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.type, "application/json");
+          assert.equal(res.body.name, "Giovanni");
+          assert.equal(res.body.surname, "da Verrazzano");
+          done();
+        });
     });
   });
 });
 
 const Browser = require('zombie');
-
+Browser.site = 'http://localhost:3000';
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
 
-
+  const browser = new Browser();
+  suiteSetup(function (done) {
+    return browser.visit('/', done);
+  });
 
   suite('Headless browser', function () {
-    test('should have a working "site" property', function() {
+    test('should have a working "site" property', function () {
       assert.isNotNull(browser.site);
     });
   });
@@ -77,15 +80,27 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Colombo').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Cristoforo');
+          browser.assert.text('span#surname', 'Colombo');
+          browser.assert.elements('span#dates', 1);
+          done();
+        });
+      });
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Vespucci').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Amerigo');
+          browser.assert.text('span#surname', 'Vespucci');
+          browser.assert.elements('span#dates', 1);
+          done();
+        });
+      });
     });
   });
 });
