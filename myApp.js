55941f9f4709a617a964e0036280f354bb4e7500
Randell Dawson <randy@randydawson.com>
2022-07-31 15:07:05 -0600

    fix: completed helmet challenges

--- myApp.js
+++ myApp.js
@@ -1,45 +1,24 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
 
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({action: 'deny'}));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
 
+const ninetyDaysInSeconds = 90*24*60*60;
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true }));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "trusted-cdn.com"]
+  }
+}));
 
 
 
