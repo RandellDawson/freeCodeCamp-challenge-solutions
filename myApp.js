71fdcd103d9f01d8251095bfbb9e94013e079513
Randell Dawson <randy@randydawson.com>
2022-08-02 15:46:30 -0600

    fix: added useUnifiedTopology

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 
